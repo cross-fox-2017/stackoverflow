b0b22716b701e11f12d062c8c77a847791f55eb7
Syanmil <syanmil@yahoo.com>
2017-02-16 19:06:07 +0700

    list all question

--- client/main.js
+++ client/main.js
@@ -12,7 +12,27 @@ function getAllQuestion(){
       if(data == 'please-login'){
         window.location.href = '../'
       } else {
-        
+        data.forEach(function(item){
+          console.log(item);
+          $('#list-of-questions').append(
+            `<tr>
+              <td>
+              <div class="ui left labeled button" tabindex="0">
+                <p class="ui basic label">
+                  ${item.title}
+                </p>
+                <div class="ui button blue">
+                  <i class="reply icon"></i> Answer
+                </div>
+              </div>
+              </td>
+              <td>${item.content}</td>
+              <td>${item.answer.length}</td>
+              <td>${item.upvote.length}</td>
+              <td>${item.downvote.length}</td>
+            </tr>`
+          )
+        })
       }
     }
   })
