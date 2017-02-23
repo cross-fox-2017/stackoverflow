bdf471b9e0996f5a9a80a7e5e61117772b67f5f8
alexanderHT <alexanderhendrawan@gmail.com>
2017-02-24 00:58:07 +0700

    pull

--- server/controllers/controller.user.js
+++ server/controllers/controller.user.js
@@ -46,7 +46,7 @@ var objUser = {
         // generate token
         if (passwordHash.verify(req.body.password, data.password)) {
           var token = jwt.sign({ username: data.username }, 'secret', { expiresIn: '1h' })
-          res.json({msgCode: 1, token})
+          res.json({msgCode: 1, token, username: data.username, userid: data.id})
         }else {
           res.json({msgCode: 0, msg: 'worng password'})
         }
